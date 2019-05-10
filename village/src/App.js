02e7612c0da4d06dc2f9dc3e89f4364097d2545d
Leonel <leonelaf12@gmail.com>
2019-05-10 12:46:22 -0400

    made my routes

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, 
+  NavLink } from 'react-router-dom';
 
 
 import './App.css';
@@ -35,8 +37,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addNewSmurf = {this.addNewSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route
+        path = '/smurf-form' 
+        render = {props => (<SmurfForm {...props} addNewSmurf = {this.addNewSmurf} />)}
+        />
+        <Route
+        exact path = '/' render = {props => <Smurfs smurfs = {this.state.smurfs}/>}
+        />
       </div>
     );
   }
